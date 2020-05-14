bdaf764f625d725d5fcd603c25451cf31ae3f748
Carlos Raiol <caremanuel@gmail.com>
2020-05-14 12:29:32 -0300

    Utilizando Webpack

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+const App = () => {
+    return (
+        <div>
+            <p>Digital Innovation One</p>
+            <p>Bem-vindo a nossa aula :)</p>
+            <p>Webpack 4</p>
+            <button>Ok</button>
+        </div>
+    )
+};
+export default App;
